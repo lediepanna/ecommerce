3740ec5f53cdfad66f60e8ebbde71c5497046f8b
Quyen <diepannanhatrang@gmail.com>
2026-06-10 18:26:19 +0200

    Disable security filters in AuthControllerTest to pass CI

--- src/test/java/com/cloudstore/ecommerce/AuthControllerTest.java
+++ src/test/java/com/cloudstore/ecommerce/AuthControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -11,18 +12,20 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class AuthControllerTest {
+class AuthControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
+    @WithAnonymousUser
     void testLoginPage() throws Exception {
         mockMvc.perform(get("/login"))
                 .andExpect(status().isOk());
     }
 
     @Test
+    @WithAnonymousUser
     void testRegisterPage() throws Exception {
         mockMvc.perform(get("/register"))
                 .andExpect(status().isOk());
